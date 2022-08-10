d595aaaa3a8c61ba397fc6e4ea32b8683df03f05
Travis Geers <armyman1992@hotmail.com>
2022-08-10 10:49:11 -0600

    added highscores.html/ created html var's, quiz start function, display question function, correct/incorrect function to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
+var questionsContainerEl = document.querySelector("#questions");
+var promptEl = document.querySelector("#question-prompt");
+var choicesEl = document.querySelector("#question-choices");
+var finalScoreEl = document.querySelector("#final-score");
+var timerEl = document.querySelector("#countdown");
+var feedbackEl = document.getElementById("right-wrong");
+var highScoreScreenEl = document.getElementById("highscore-input");
+var initialsEl = document.getElementById("initials");
+var startBtn = document.querySelector("#start");
+var submitBtn = document.querySelector("#submit");
+
 var currentQuestion = 0;
 var time = 100;
 var timer;
@@ -31,5 +42,40 @@ var questions = [
     }
 ];
 
+// Quiz Start Function
+var startQuiz = function() {
+    var startInfoEl = document.querySelector(".start-info");
+    startInfoEl.setAttribute("class", "hide");
+    questionsContainerEl.removeAttribute("class");
+    timer = setInterval(clock, 1000);
+    timerEl.textContent = time;
+    displayQuestion();
+}
+
+// Display Question Function
+var displayQuestion = function() {
+    var questionIndex = questions[currentQuestion];
+    promptEl.textContent = questionIndex.prompt;
+    choicesEl.innerHTML = "";
+    questionIndex.choices.forEach(function(choices, i) {
+        var choiceBtn = document.createElement("button");
+        choiceBtn.setAttribute("class", "choice btn btn-outline-primary")
+        choiceBtn.setAttribute("value", choices);
+        choiceBtn.textContent = i + 1 + ". " + choices;
+        choiceBtn.onclick = choiceClick;
+        choicesEl.appendChild(choiceBtn);
+    });
+};
+
+// Correct/Incorrect response. Function will 
+var choiceClick = function() {
+    if (this.value !== questions[currentQuestion].answer) {
+        time -= 10;
+        timerEl.textContent = time;
+        feedbackEl.textContent = "Incorrect!"
+    } else {
+        feedbackEl.textContent = "Correct!";
+    }
+};
 
 
